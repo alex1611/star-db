54eec8278c81d1a5e68bbe08440c8a658358f027
alex <alex42456@gmail.com>
2019-08-04 22:33:04 +0300

    random planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -6,50 +6,65 @@ import Spinner from "../spinner/spinner";
 class RandomPlanet extends Component {
     swapiService = new SwapiService();
     state = {
+        loading: true,
         planet: {}
     };
 
-    constructor(){
+    constructor() {
         super();
         this.updatePlanet();
     }
 
     updatePlanet() {
-        const id = Math.floor(Math.random()*25 + 2);
+        const id = Math.floor(Math.random() * 25 + 2);
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoad);
     }
 
-    onPlanetLoad = (planet)=> {
-        this.setState({planet});
+    onPlanetLoad = (planet) => {
+        this.setState({
+            planet,
+            loading: false
+        });
     };
 
     render() {
-        const {planet: {id, name, population, rotationPeriod, diameter}} = this.state;
-        return <Spinner/>;
+        const {planet, loading} = this.state;
+        const spinner = loading? <Spinner/>: null;
+        const content = !loading? <PlanetView planet={planet}/>: null;
         return (
             <div className="random-planet jumbotron rounded">
-                <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                     className="planet-image"/>
-                <h4>{name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">
-                        <span className="term">Population</span>
-                        <span>{population}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Rotation Period</span>
-                        <span>{rotationPeriod}</span>
-                    </li>
-                    <li className="list-group-item">
-                        <span className="term">Diameter</span>
-                        <span>{diameter}</span>
-                    </li>
-                </ul>
+                {spinner}
+                {content}
             </div>
         );
     }
 }
 
+const PlanetView = ({planet}) => {
+        const {id, name, population, rotationPeriod, diameter} = planet;
+    return (
+        <React.Fragment>
+            <img src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+                 className="planet-image"/>
+            < h4 > {name}
+            </h4>
+            <ul className="list-group list-group-flush">
+                <li className="list-group-item">
+                    <span className="term">Population</span>
+                    <span>{population}</span>
+                </li>
+                <li className="list-group-item">
+                    <span className="term">Rotation Period</span>
+                    <span>{rotationPeriod}</span>
+                </li>
+                <li className="list-group-item">
+                    <span className="term">Diameter</span>
+                    <span>{diameter}</span>
+                </li>
+            </ul>
+        </React.Fragment>
+    )
+}
 export default RandomPlanet;
--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,18 +1,13 @@
 import React, {Component} from 'react';
 
 const Spinner = () => {
+    const styles = {
+        width: '100%',
+        height: '100%',
+    };
     return (
-        <div className="lds-css ng-scope">
-            <div className="lds-blocks" style={"width:100%;height:100%"}>
-                <div style={"left:38px;top:38px;animation-delay:0s"}></div>
-                <div style="left:80px;top:38px;animation-delay:0.125s"></div>
-                <div style="left:122px;top:38px;animation-delay:0.25s"></div>
-                <div style="left:38px;top:80px;animation-delay:0.875s"></div>
-                <div style="left:122px;top:80px;animation-delay:0.375s"></div>
-                <div style="left:38px;top:122px;animation-delay:0.75s"></div>
-                <div style="left:80px;top:122px;animation-delay:0.625s"></div>
-                <div style="left:122px;top:122px;animation-delay:0.5s"></div>
-            </div>
+        <div>
+            Loading...
         </div>
     );
 };
